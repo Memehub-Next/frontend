1fd953d35bc95aa0d1b16e5c749e13afc5c76af7
JeremySazMining <54854447+Distortedlogic@users.noreply.github.com>
2023-02-01 16:08:51 -0500

    Update nextUrqlClient.ts

--- src/graphql/urql-client/nextUrqlClient.ts
+++ src/graphql/urql-client/nextUrqlClient.ts
@@ -3,25 +3,21 @@ import { requestPolicyExchange } from "@urql/exchange-request-policy";
 import { NextUrqlClientConfig } from "next-urql";
 import { dedupExchange, fetchExchange } from "urql";
 import { getSubscriptionExchange } from ".";
-import { Environment } from "../../utils/environment";
 import { errorExchange } from "./index";
 import { normalizedCache } from "./normalizedCache";
 
-export const nextUrqlClient: NextUrqlClientConfig = (ssrExchange, ctx) => {
-  const headers = Environment.isServer() && ctx?.req?.headers ? ctx.req.headers : (undefined as any);
-  return {
-    url: process.env.NEXT_PUBLIC_GQL_URL,
-    fetchOptions: { credentials: "include" as const, headers },
-    exchanges: [
-      devtoolsExchange,
-      dedupExchange,
-      requestPolicyExchange({}),
-      normalizedCache,
-      errorExchange,
-      ssrExchange,
-      // persistedFetchExchange({}),
-      ...getSubscriptionExchange(),
-      fetchExchange,
-    ],
-  };
-};
+export const nextUrqlClient: NextUrqlClientConfig = (ssrExchange, ctx) => ({
+  url: process.env.NEXT_PUBLIC_GQL_URL,
+  fetchOptions: { credentials: "include" as const, headers: { cookie: ctx?.req?.headers.cookie ?? "" } },
+  exchanges: [
+    devtoolsExchange,
+    dedupExchange,
+    requestPolicyExchange({}),
+    normalizedCache,
+    errorExchange,
+    ssrExchange,
+    // persistedFetchExchange({}),
+    ...getSubscriptionExchange(),
+    fetchExchange,
+  ],
+});
